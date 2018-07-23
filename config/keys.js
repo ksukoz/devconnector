6e122a8fe9f1dadc97c82d3f192eab892667efab
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-23 22:33:31 +0300

    Implement token for user login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ksu:ksu113@ds145921.mlab.com:45921/devconnector-113"
+  mongoURI: "mongodb://ksu:ksu113@ds145921.mlab.com:45921/devconnector-113",
+  secretOrKey: "secret"
 };
