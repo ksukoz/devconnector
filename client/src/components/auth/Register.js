1e3a75602d36ef26277804419a115b2bcb842351
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-26 22:12:40 +0300

    Add conditional navbar links and logout functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -37,6 +37,12 @@ class Register extends Component {
     this.props.registerUser(newUser, this.props.history);
   };
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   render() {
     const { errors } = this.state;
 
