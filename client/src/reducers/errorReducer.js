249b3135373d8617de0f0cad4d46d322818ff997
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-26 08:47:05 +0300

    Implement registration and error handling with Redux

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,4 +1,4 @@
-import { TEST_DISPATCH } from "../actions/types";
+import { GET_ERRORS } from "../actions/types";
 
 const initialState = {
   isAuthenticated: false,
@@ -7,6 +7,8 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case GET_ERRORS:
+      return action.payload;
     default:
       return state;
   }
